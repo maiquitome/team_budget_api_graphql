9e06be388c11e013547b95d5a6cd9c1413225782
Maiqui Pirolli Tomé <maiquitome@gmail.com>
2021-06-03 19:32:51 -0300

    :seedling: update seeds

--- priv/repo/seeds.exs
+++ priv/repo/seeds.exs
@@ -1,13 +1,31 @@
 alias TeamBudget.{Accounts.User, Repo}
 
-%{first_name: "Maiqui", last_name: "Tomé", email: "maiqui@teste.com", password_hash: "123456"}
+%{
+  first_name: "Maiqui",
+  last_name: "Tomé",
+  email: "maiqui@teste.com",
+  password: "123456",
+  password_confirmation: "123456"
+}
 |> User.changeset()
-|> Repo.insert()
+|> Repo.insert!()
 
-%{first_name: "Mike", last_name: "Shinoda", email: "mike@teste.com", password_hash: "12345678"}
+%{
+  first_name: "Mike",
+  last_name: "Shinoda",
+  email: "mike@teste.com",
+  password: "12345678",
+  password_confirmation: "12345678"
+}
 |> User.changeset()
-|> Repo.insert()
+|> Repo.insert!()
 
-%{first_name: "Mayk", last_name: "Brito", email: "mayk@teste.com", password_hash: "12345678910"}
+%{
+  first_name: "Mayk",
+  last_name: "Brito",
+  email: "mayk@teste.com",
+  password: "12345678910",
+  password_confirmation: "12345678910"
+}
 |> User.changeset()
-|> Repo.insert()
+|> Repo.insert!()
